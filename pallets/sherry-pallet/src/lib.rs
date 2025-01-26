67d4224d319c4c4d0c2b78a885b39ab0e4629589
sherry <sherryplutus@gmail.com>
2025-01-27 00:32:46 +0500

    added sherry pallet lib

--- pallets/sherry-pallet/src/lib.rs
+++ pallets/sherry-pallet/src/lib.rs
@@ -1,3 +1,95 @@
-fn main() {
-    println!("Hello, world!");
+//! # Cherry Count Pallet
+//!
+//! A pallet that demonstrates simple storage and extrinsic functions in Substrate.
+//! This pallet maintains a single storage item `CherryCount` and provides extrinsics to add to its value
+//! and retrieve the current count.
+//! 
+//! 
+//! 1. Pallet Config
+//! 2. Pallet Storage
+//! 3. Pallet Event
+//! 4. Pallet Error
+//! 5. Pallet Calls
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+pub use pallet::*;
+
+#[frame_support::pallet]
+pub mod pallet {
+    use frame_support::pallet_prelude::*;
+    use frame_system::pallet_prelude::*;
+
+    #[pallet::pallet]
+    pub struct Pallet<T>(_);
+
+    #[pallet::config]
+    pub trait Config: frame_system::Config {
+        /// The overarching event type.
+        type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+    }
+
+    /// Storage item for the example pallet.
+    #[pallet::storage]
+    #[pallet::getter(fn stored_value)]
+    pub type StoredValue<T> = StorageValue<_, Option<u32>, ValueQuery>;
+
+
+    /// Events emitted by this pallet.
+    #[pallet::event]
+    #[pallet::generate_deposit(pub(super) fn deposit_event)]
+    pub enum Event<T: Config> {
+        /// A new value was set.
+        ValueSet { value: u32, who: T::AccountId },
+        /// The value was incremented.
+        ValueIncremented { new_value: u32 },
+    }
+
+    /// Errors returned by this pallet.
+    #[pallet::error]
+    pub enum Error<T> {
+        /// The storage value is uninitialized.
+        NoValue,
+        /// The value overflowed when incrementing.
+        Overflow,
+    }
+
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {
+        /// Set a new value in storage.
+        #[pallet::call_index(0)]
+        #[pallet::weight(10_000)]
+        pub fn set_value(origin: OriginFor<T>, value: u32) -> DispatchResult {
+            let who = ensure_signed(origin)?;
+            // Wrap the value in `Some` since the storage type is `Option<u32>`
+            StoredValue::<T>::put(Some(value));
+            Self::deposit_event(Event::ValueSet { value, who });
+            Ok(())
+        }
+
+        /// Increment the stored value.
+#[pallet::call_index(1)]
+#[pallet::weight(10_000)]
+pub fn increment_value(origin: OriginFor<T>) -> DispatchResult {
+    let _who = ensure_signed(origin)?;
+
+    // Mutate the stored value
+    StoredValue::<T>::try_mutate(|maybe_value| {
+        match maybe_value {
+            None => Err(Error::<T>::NoValue.into()), // Return an error if no value exists
+            Some(v) => {
+                // Increment the value safely, checking for overflow
+                *v = v.checked_add(1).ok_or(Error::<T>::Overflow)?;
+                Self::deposit_event(Event::ValueIncremented { new_value: *v });
+                Ok(())
+            }
+        }
+    })
+}
+
+
+
+
+
+    }
 }
